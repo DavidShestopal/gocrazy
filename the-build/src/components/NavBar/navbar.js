bf77b01ad0a0683366542923b885c34d21cfcac4
David Shestopal <dshestopal@gmail.com>
2020-10-09 16:01:39 -0700

    nav bar change

--- the-build/src/components/NavBar/navbar.js
+++ the-build/src/components/NavBar/navbar.js
@@ -7,13 +7,15 @@ import { BrowserRouter, Route, Link } from 'react-router-dom';
 class NavBar extends React.Component {
   state = {
     navBackground: 'transparent',
+    wordColor: 'black',
   };
 
   componentDidMount() {
     document.addEventListener('scroll', () => {
-      const backgroundcolor = window.scrollY < 70 ? 'transparent' : 'blue';
+      const backgroundcolor = window.scrollY < 70 ? 'transparent' : 'black';
+      const color = window.scrollY < 70 ? 'black' : 'white';
 
-      this.setState({ navBackground: backgroundcolor });
+      this.setState({ navBackground: backgroundcolor, wordColor: color });
     });
   }
   render() {
@@ -27,18 +29,33 @@ class NavBar extends React.Component {
             expand="lg"
             style={{
               backgroundColor: `${this.state.navBackground}`,
+
               position: 'fixed',
               top: '0px',
               width: '100%',
+              borderBottom: '1px solid grey',
             }}>
-            <Navbar.Brand> Stranger</Navbar.Brand>
+            <h3
+              style={{ fontStyle: 'italic', fontWeight: 'bolder', fontSize: '2rem', color: `${this.state.wordColor}` }}>
+              {' '}
+              Stranger
+            </h3>
 
             <Navbar.Toggle className="border-0" aria-controls="navbar-toggle" />
             <Navbar.Collapse id="navbar-toggle">
               <Nav className="ml-auto">
                 <div>
-                  <a href="/html/">HTML</a> <a href="/css/">CSS</a> <a href="/js/">JavaScript</a>
-                  <a href="/python/">Python</a>
+                  <a
+                    href="/about"
+                    style={{ color: `${this.state.wordColor}`, marginRight: '1rem', fontWeight: 'bold' }}>
+                    About
+                  </a>{' '}
+                  <a href="/shop" style={{ color: `${this.state.wordColor}`, marginRight: '1rem', fontWeight: 'bold' }}>
+                    Shop
+                  </a>{' '}
+                  <a href="/Cart" style={{ color: `${this.state.wordColor}`, marginRight: '1rem', fontWeight: 'bold' }}>
+                    Cart
+                  </a>
                 </div>
               </Nav>
             </Navbar.Collapse>
